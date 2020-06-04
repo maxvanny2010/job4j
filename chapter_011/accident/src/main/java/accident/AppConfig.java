b09d961fcb3426b8ec2f5d9d2467c3974fb412b0
Maxim <mailtime2010@gmail.com>
2020-06-04 07:39:05 -0400

    MVC.0 [6877#205783]

---
+++ chapter_011/accident/src/main/java/accident/AppConfig.java
@@ -0,0 +1,34 @@
+package accident;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * AppConfig.
+ *
+ * @author Maxim Vanny
+ * @version 5.0
+ * @since 6/4/2020
+ */
+@Configuration
+@ComponentScan("accident")
+public class AppConfig {
+    /**
+     * Method to a view resolver.
+     *
+     * @return bean
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        final InternalResourceViewResolver bean =
+                new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("./WEB-INF/view/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
---
+++ chapter_011/accident/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Maxim Vanny
+  Date: 6/4/2020
+  Time: 6:46 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Accident</title>
+</head>
+<body>
+Hello : Accident
+</body>
+</html>
